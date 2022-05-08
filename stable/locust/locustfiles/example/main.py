f094b1b4aab6ee1dfa525a60869dfe03bdeb4839
dimitry <dimitrys@rafael.co.il>
2022-05-08 14:40:44 +0300

    feat(automation):
    + putting workdir

--- stable/locust/locustfiles/example/main.py
+++ stable/locust/locustfiles/example/main.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from locust import HttpUser, task, between
-from lib.example_functions import choose_random_page
-
-
+# from lib.example_functions import choose_random_page
+import random
+import os
 default_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
 
+
+
+
+def choose_random_page():
+    pages = [
+        '/policies/privacy/',
+        '/contact/',
+        '/about/',
+        '/search/howsearchworks/crawling-indexing/',
+        '/search/howsearchworks/algorithms/'
+    ]
+
+    return random.choice(pages)
 
 class WebsiteUser(HttpUser):
     wait_time = between(1, 2)
@@ -13,7 +26,10 @@ class WebsiteUser(HttpUser):
     @task(1)
     def get_index(self):
         self.client.get("/", headers=default_headers)
+        print(os.getcwd())
 
     @task(3)
     def get_random_page(self):
         self.client.get(choose_random_page(), headers=default_headers)
+
+
