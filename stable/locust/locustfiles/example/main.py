58fff328452c82c906bbe7eb20f1eac2a16d529c
dimitry <dimitrys@rafael.co.il>
2022-05-10 15:51:07 +0300

    chore(automation):
    + changing image tag for argo

--- stable/locust/locustfiles/example/main.py
+++ stable/locust/locustfiles/example/main.py
@@ -4,6 +4,7 @@ from locust import HttpUser, task, between
 # from lib.example_functions import choose_random_page
 import random
 import os
+import glob
 default_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
 
 
@@ -26,7 +27,8 @@ class WebsiteUser(HttpUser):
     @task(1)
     def get_index(self):
         self.client.get("/", headers=default_headers)
-        print(os.getcwd())
+        print(glob.glob(os.getcwd()))
+
 
     @task(3)
     def get_random_page(self):
