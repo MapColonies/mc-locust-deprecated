245ca77a4ffb61a8b95f531bb7d78f209eb3b6a8
dimitry <dimitrys@rafael.co.il>
2022-05-24 17:04:10 +0300

    chore(automation):
    + test

--- common/config.py
+++ common/config.py
@@ -23,6 +23,6 @@ path_builder = f"{layer_type}/{layer}/{projection}/TileMatrix/TileCol/TileRow{im
 
 default_headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
-    'X-API-KEY': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwicmVzb3VyY2VUeXBlcyI6WyJyYXN0ZXIiLCJkZW0iLCJ2ZWN0b3IiLCIzZCJdLCJpYXQiOjE1MTYyMzkwMjJ9.kidhXiB3ihor7FfkaduJxpJQXFMJGVH9fH7WI6GLGM0'}
+    'X-API-KEY': ''}
 
 
