1775d9dbb9b8a197ea3703a1fdeacc47e6b9c674
SHAYAVR <SHAYAVR@rafael.co.il>
2022-05-30 19:17:32 +0300

    check json

--- common/Log.py
+++ common/Log.py
@@ -1,6 +1,23 @@
 import logging
 import os
 import time
+from datetime import datetime
+
+from pythonjsonlogger import jsonlogger
+
+
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+        if not log_record.get('timestamp'):
+            # this doesn't use record.created, so it is slightly off
+            now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            log_record['timestamp'] = now
+        if log_record.get('level'):
+            log_record['level'] = log_record['level'].upper()
+        else:
+            log_record['level'] = record.levelname
+
 
 LEVELS = {
     'debug': logging.DEBUG,
@@ -24,11 +41,21 @@ def create_file(filename):
     else:
         pass
 
+def create_json_formatter(logs_format):
+    j_formatter = CustomJsonFormatter(logs_format)
+    return j_formatter
 
-def set_handler(levels):
+def set_file_handler(levels):
     if levels == 'error':
         logger.addHandler(MyLog.err_handler)
     logger.addHandler(MyLog.handler)
+
+
+def set_stream_handler(levels, logs_format):
+    if levels == 'error':
+        handler = MyLog.stream_handler
+        handler.setFormatter(create_json_formatter(logs_format))
+        logger.addHandler(MyLog.stream_handler)
 
 
 def remove_handler(levels):
@@ -41,7 +68,10 @@ def get_current_time():
     return time.strftime(MyLog.date, time.localtime(time.time()))
 
 
-class MyLog:
+class MyLog(CustomJsonFormatter):
+    def __init__(self):
+        super().__init__()
+
     path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     log_file = path + '/Log/log.log'
     err_file = path + '/Log/err.log'
@@ -51,34 +81,48 @@ class MyLog:
     date = '%Y-%m-%d %H:%M:%S'
 
     handler = logging.FileHandler(log_file, encoding='utf-8', mode='w')
+    stream_handler = logging.StreamHandler()
     err_handler = logging.FileHandler(err_file, encoding='utf-8', mode='w')
 
     @staticmethod
     def debug(log_meg):
-        set_handler('debug')
+        set_file_handler('debug')
         logger.debug("[DEBUG " + get_current_time() + "]" + log_meg)
         remove_handler('debug')
 
     @staticmethod
     def info(log_meg):
-        set_handler('info')
+        set_file_handler('info')
         logger.info("[INFO " + get_current_time() + "]" + log_meg)
         remove_handler('info')
 
     @staticmethod
     def warning(log_meg):
-        set_handler('warning')
+        set_file_handler('warning')
         logger.warning("[WARNING " + get_current_time() + "]" + log_meg)
         remove_handler('warning')
 
     @staticmethod
     def error(log_meg):
-        set_handler('error')
+        set_file_handler('error')
         logger.error("[ERROR " + get_current_time() + "]" + log_meg)
         remove_handler('error')
 
     @staticmethod
     def critical(log_meg):
-        set_handler('critical')
+        set_file_handler('critical')
         logger.error("[CRITICAL " + get_current_time() + "]" + log_meg)
         remove_handler('critical')
+
+
+
+    @staticmethod
+    def stream_debug(log_meg, log_fmt):
+        set_stream_handler('error',logs_format=log_fmt)
+        logger.error(log_meg)
+        remove_handler('error')
+
+
+shay = MyLog.stream_debug("success",'%(timestamp)s %(level)s %(name)s %(message)s' )
+
+
