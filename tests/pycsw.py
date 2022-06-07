5f95c52b17993a2cadc5299effccb4b3fc24426c
dimitry <dimitrys@rafael.co.il>
2022-06-07 12:00:08 +0300

    update image tag

--- tests/pycsw.py
+++ tests/pycsw.py
@@ -1,3 +1,4 @@
+from email import header
 import os
 import requests
 # import xmltodict
@@ -6,6 +7,7 @@ from common.config import REQUEST_HEADER, PARAMS, PORT, HOST
 
 url = 'https://pycsw-qa-pycsw-route-raster.apps.v0h0bdx6.eastus.aroapp.io/?service=CSW&request=GetRecordById&typenames=mc:MCRasterRecord&ElementSetName=full&resultType=results&outputSchema=http://schema.mapcolonies.com/raster&version=2.0.2&id=3fc674cd-7b77-40ac-8fa3-96b6b4c77f3b'
 
-response = requests.get(url=url, params=PARAMS, headers=REQUEST_HEADER)
-print(response)
+print(REQUEST_HEADER)
+# response = requests.get(url=url, params=PARAMS, headers=REQUEST_HEADER)
+# print(response)
 # print(os.environ.get('DEFAULT_HEADERS'))
