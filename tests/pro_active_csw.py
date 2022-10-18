04c6765fe732fabcd1d3e5dc8d815e4b1045409a
dimitry <dimitrys@rafael.co.il>
2022-10-18 16:21:38 +0300

    feat: update latest tests - pro active

---
+++ tests/pro_active_csw.py
@@ -0,0 +1,166 @@
+from bson import encode
+from locust import between
+from locust import constant
+from locust import constant_pacing
+from locust import constant_throughput
+from locust import HttpUser
+from locust import task
+from locust import user
+
+import common.config as cfg
+
+# Environment variables for XML requests
+MC_ID_PROPERTY = cfg.PYCSW_ID_PROPERTY
+MC_ID_VALUE = cfg.PYCSW_ID_VALUE
+MC_POLYGON_PROPERTY = cfg.PYCSW_POLYGON_PROPERTY
+MC_POLYGON_VALUE = cfg.PYCSW_POLYGON_VALUE
+MC_REGION_PROPERTY = cfg.PYCSW_REGION_PROPERTY
+MC_REGION_VALUE = cfg.PYCSW_REGION_VALUE
+
+# Weights for the different tasks
+FIRST_WEIGHT = cfg.FIRST_TASK_WEIGHT
+SECOND_TASK_WEIGHT = cfg.SECOND_TASK_WEIGHT
+THIRD_TASK_WEIGHT = cfg.THIRD_TASK_WEIGHT
+FOURTH_TASK_WEIGHT = cfg.FOURTH_TASK_WEIGHT
+
+# URLs for the relevant tasks
+PYCSW_TARGET_URL = cfg.PYCSW_HOST
+
+# Constants for XML post requests
+POLYGON_XML = rf"""<?xml version="1.0" encoding="UTF-8"?>
+
+<csw:GetRecords xmlns:csw="http://www.opengis.net/cat/csw/2.0.2" service="CSW" maxRecords="1" startPosition="1" outputSchema="http://schema.mapcolonies.com/raster" version="2.0.2" xmlns:mc="http://schema.mapcolonies.com/raster" >
+
+<csw:Query typeNames="mc:MCRasterRecord">
+
+<csw:ElementSetName>brief</csw:ElementSetName>
+
+<csw:Constraint version="1.1.0">
+
+<Filter xmlns="http://www.opengis.net/ogc">
+
+<PropertyIsLike wildCard="%" singleChar="_" escapeChar="\\">
+
+<PropertyName>{MC_POLYGON_PROPERTY}</PropertyName>
+
+<Literal>{MC_POLYGON_VALUE}</Literal>
+
+</PropertyIsLike>
+
+</Filter>
+
+</csw:Constraint>
+
+</csw:Query>
+
+</csw:GetRecords>
+"""
+
+
+ID_RECORD_XML = rf"""<?xml version="1.0" encoding="UTF-8"?>
+
+<csw:GetRecords xmlns:csw="http://www.opengis.net/cat/csw/2.0.2" service="CSW" maxRecords="1" startPosition="1" outputSchema="http://schema.mapcolonies.com/raster" version="2.0.2" xmlns:mc="http://schema.mapcolonies.com/raster" >
+
+<csw:Query typeNames="mc:MCRasterRecord">
+
+<csw:ElementSetName>brief</csw:ElementSetName>
+
+<csw:Constraint version="1.1.0">
+
+<Filter xmlns="http://www.opengis.net/ogc">
+
+<PropertyIsLike wildCard="%" singleChar="_" escapeChar="\\">
+
+<PropertyName>{MC_ID_PROPERTY}</PropertyName>
+
+<Literal>{MC_ID_VALUE}</Literal>
+
+</PropertyIsLike>
+
+</Filter>
+
+</csw:Constraint>
+
+</csw:Query>
+
+</csw:GetRecords>
+"""
+
+
+REGION_RECORD_XML = rf"""<?xml version="1.0" encoding="UTF-8"?>
+
+<csw:GetRecords xmlns:csw="http://www.opengis.net/cat/csw/2.0.2" service="CSW" maxRecords="1" startPosition="1" outputSchema="http://schema.mapcolonies.com/raster" version="2.0.2" xmlns:mc="http://schema.mapcolonies.com/raster" >
+
+<csw:Query typeNames="mc:MCRasterRecord">
+
+<csw:ElementSetName>brief</csw:ElementSetName>
+
+<csw:Constraint version="1.1.0">
+
+<Filter xmlns="http://www.opengis.net/ogc">
+
+<PropertyIsLike wildCard="%" singleChar="_" escapeChar="\\">
+
+<PropertyName>{MC_REGION_PROPERTY}</PropertyName>
+
+<Literal>{MC_REGION_VALUE}</Literal>
+
+</PropertyIsLike>
+
+</Filter>
+
+</csw:Constraint>
+
+</csw:Query>
+
+</csw:GetRecords>
+"""
+
+
+class SizingUser(HttpUser):
+
+    if cfg.WAIT_FUNCTION == 1:
+        wait_time = constant(cfg.WAIT_TIME)
+        print("Choosing constant wait time")
+    elif cfg.WAIT_FUNCTION == 2:
+        wait_time = constant_throughput(cfg.WAIT_TIME)
+        print("Choosing constant throughput wait time")
+    elif cfg.WAIT_FUNCTION == 3:
+        wait_time = between(cfg.MIN_WAIT, cfg.MAX_WAIT)
+        print("Choosing between wait time")
+    elif cfg.WAIT_FUNCTION == 4:
+        wait_time = constant_pacing(cfg.WAIT_TIME)
+        print("Choosing constant pacing wait time")
+    else:
+        print("Invalid wait function")
+
+    @task(1)
+    def get_records_by_polygon(self):
+        r1 = self.client.post(
+            "/",
+            data=POLYGON_XML.encode("UTF-8"),
+            headers=cfg.REQUEST_HEADER,
+            verify=False,
+        )
+
+    @task(1)
+    def get_records_by_id(self):
+        r2 = self.client.post(
+            "/",
+            data=ID_RECORD_XML.encode("utf-8"),
+            headers=cfg.REQUEST_HEADER,
+            verify=False,
+        )
+
+    @task(1)
+    def get_records_by_region(self):
+        r3 = self.client.post(
+            "/",
+            data=REGION_RECORD_XML.encode("utf-8"),
+            headers=cfg.REQUEST_HEADER,
+            verify=False,
+        )
+
+    # @task
+    # def get_records_by_bbox(self):
+    #     pass
--- tests/pro_active_template.py
+++ tests/pro_active_template.py
@@ -3,7 +3,7 @@ from datetime import date
 from datetime import datetime
 from typing import Iterable
 from typing import Iterator
-
+from random import shuffle
 from locust import constant
 from locust import events
 from locust import HttpUser
@@ -16,9 +16,10 @@ from common.utils import WMTSIterator
 
 # ssn_reader = [WMTSIterator(1, 5+1), WMTSIterator(5, 10), WMTSIterator(10, 15)]
 
-ssn_reader = WMTSIterator(range(0, 5))
+# ssn_reader = WMTSIterator(range(0, 5))
 
-
+lst = list(range(1, 11))
+# shuffle(lst)
 now = datetime.now()
 
 # count = range(0,5)
@@ -29,14 +30,16 @@ now = datetime.now()
 def on_test_start(environment, **kwargs):
     current_time = now.strftime("%H:%M:%S")
     today = date.today()
-    print(f"Test Started \nCurrent Time : {current_time} \nToday's date: {today} ")
+    print(
+        f"Test Started \nCurrent Time : {current_time} \nToday's date: {today} ")
 
 
 @events.test_stop.add_listener
 def on_test_stop(environment, **kwargs):
     current_time = now.strftime("%H:%M:%S")
     today = date.today()
-    print(f"Test Ended \nCurrent Time : {current_time} \nToday's date: {today} ")
+    print(
+        f"Test Ended \nCurrent Time : {current_time} \nToday's date: {today} ")
 
 
 # class ProActiveUser(HttpUser):
@@ -51,8 +54,9 @@ class MyTaskSet(TaskSet):
     @task(1)
     def fast(self):
         self.client.get("/", name="fast_check")
-        points_1 = next(ssn_reader)
-        print(points_1)
+        # points_1 = next(ssn_reader)
+        shuffle(lst)
+        print(lst[0])
         # points_2 = next(ssn_reader[1])
         # points_3 = next(ssn_reader[2])
         # print(f'{points_1} ,{points_2}, {points_3}')
@@ -66,5 +70,5 @@ class MyTaskSet(TaskSet):
 
 
 class MyLocust(HttpUser):
-    wait_time = constant(2)
+    wait_time = constant(30)
     tasks = [MyTaskSet]
